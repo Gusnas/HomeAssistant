15323b086efc8849dec5d95128d6a313fc7e8dd0
Gustavo Nascimento <gusnas@alu.ufc.br>
2022-01-13 23:07:12 -0300

    Criação do Client e do Server

---
+++ HomeAssistant/HomeAssistant/Usage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAssistant
+{
+    class Usage
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server();
+            server.Initialize("127.0.0.1", 27000);
+
+            Client client = new Client();
+            client.Initialize("127.0.0.1", 27000);
+            client.Send("Testing!");
+
+            Console.ReadKey();
+        }
+    }
+}
